046892643e72122073a68b14bd61375cbfca89cc
Jaro <p.jaronsky@gmail.com>
2020-02-14 18:40:10 +0100

    Refactor

--- webpack.config.d/webpack.config.js
+++ webpack.config.d/webpack.config.js
@@ -1,16 +1,27 @@
-config.devServer = config.devServer || {}; // create devServer in case it is undefined
+path = require("path")
+
+config = config || {};
+config.devServer = config.devServer || {};
 config.devServer = {
     "hot": true,
     "open": false,
     "port": 3000,
-    "contentBase": [
-        "C:\\Users\\pjaronski\\IdeaProjects\\kotlin-fullstack-mpp\\build\\processedResources\\frontend\\main\\public"
+    contentBase: [
+        path.resolve(__dirname, "..\\..\\..\\processedResources\\frontend\\main\\public")
     ]
 }
+config.devServer.proxy = config.devServer.proxy || {};
 config.devServer.proxy = {
     "/api": "https://localhost:8080"
 }
+config.devServer.watchOptions = config.devServer.watchOptions || {};
 config.devServer.watchOptions = {
     "aggregateTimeout": 5000,
     "poll": 1000
 };
+
+/*module.exports = {
+    entry: {
+        main: path.resolve(__dirname, "kotlin\\kotlin-fullstack-mpp-frontend.js")
+    }
+}*/
